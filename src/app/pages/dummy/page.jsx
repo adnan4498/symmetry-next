0d6cde142f04deb5c3a3d8f618803fc33ca81ba4
Rihan Saeed <rihan.saeed@symmetrydigital.com>
2024-01-02 17:34:13 +0500

    apply this sliding code

--- src/app/pages/dummy/page.jsx
+++ src/app/pages/dummy/page.jsx
@@ -9,20 +9,25 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Page = () => {
   useEffect(() => {
-    var colors = ["#f38630", "#6fb936", "#ccc", "#6fb936"];
+    // var colors = ["#f38630", "#6fb936", "#ccc", "#6fb936"];
 
-    //initially colorize each box and position in a row
-    gsap.set(".box", {
-      backgroundColor: (i) => colors[i % colors.length],
-      x: (i) => i * 50,
-    });
+    // //initially colorize each box and position in a row
+    // gsap.set(".box", {
+    //   backgroundColor: (i) => colors[i % colors.length],
+    //   x: (i) => i * 50,
+    // });
+
+    // gsap.set(".box", {
+    //   backgroundColor: (i) => colors[i % colors.length],
+    //   x: (i) => i * 50,
+    // });
 
     gsap.to(".box", {
       duration: 5,
       ease: "none",
-      x: "+=500", //move each box 500px to right
+      x: "+=100", //move each box 500px to right
       modifiers: {
-        x: gsap.utils.unitize((x) => x % 500), //force x value to be between 0 and 500 using modulus
+        x: gsap.utils.unitize((x) => parseFloat(x) % 100), //force x value to be between 0 and 500 using modulus
       },
       repeat: -1,
     });
@@ -32,16 +37,28 @@ const Page = () => {
     <>
       <div class="wrapper">
         <div class="boxes">
-          <div class="box">1</div>
-          <div class="box">2</div>
-          <div class="box">3</div>
-          <div class="box">4</div>
+          <div class="box left-[150px]">skillfully</div>
+          <div class="box">accepting</div>
+          <div class="box right-[5px]">challanges</div>
+          {/* <div class="box">4</div>
           <div class="box">5</div>
           <div class="box">6</div>
           <div class="box">7</div>
           <div class="box">8</div>
           <div class="box">9</div>
-          <div class="box">10</div>
+          <div class="box">10</div> */}
+        </div>
+        <div class="boxes">
+          <div class="box left-[150px]">skillfully</div>
+          <div class="box">accepting</div>
+          <div class="box right-[5px]">challanges</div>
+          {/* <div class="box">4</div>
+          <div class="box">5</div>
+          <div class="box">6</div>
+          <div class="box">7</div>
+          <div class="box">8</div>
+          <div class="box">9</div>
+          <div class="box">10</div> */}
         </div>
       </div>
     </>
