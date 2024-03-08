bbcefc403bc4f3b43dac8ca200a17e60b033d5c5
adnan4498 <akhan12456008@gmail.com>
2024-03-08 17:21:24 +0500

    nav li scroll activation is done in dummy 2. apply it in nav

--- src/app/dummy2/page.jsx
+++ src/app/dummy2/page.jsx
@@ -1,21 +1,96 @@
-// "use client";
-// import React, { useRef, useState, useEffect } from "react";
-// import gsap from "gsap";
-// import GsapComp from "../components/gsapComp/GsapScrollAnimationComp"
+"use client";
+import React, { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger"; // Import ScrollTrigger
 
-// const Page = () => {
- 
-//   const gsapCompRef = GsapComp()
+import GsapComp from "../components/gsapComp/GsapScrollAnimationComp";
 
-//   return (
-//     <>
-//      <div className=" flex flex-col items-center gap-80">
-//       <div className="bg-green-400 h-[50vh] w-[50%]" >1</div>
-//       <div className="bg-blue-400 h-[50vh] w-[50%] mt-40" ref={gsapCompRef}>2</div>
-//       <div className="bg-red-400 h-[50vh] w-[50%]" ref={gsapCompRef}>3</div>
-//      </div>
-//     </>
-//   );
-// };
+gsap.registerPlugin(ScrollTrigger); // Register ScrollTrigger
 
-// export default Page;
+const Page = () => {
+  const gsapCompRef = GsapComp();
+  const liRefs = {
+    home: useRef(null),
+    about: useRef(null),
+    contact: useRef(null),
+  };
+
+  const liItems = [
+    {
+      id: 0,
+      item: "home",
+      refID: "#homeRef",
+    },
+    {
+      id: 1,
+      item: "about",
+      refID: "#aboutRef",
+    },
+    {
+      id: 2,
+      item: "contact",
+      refID: "#contactRef",
+    },
+  ];
+
+  useEffect(() => {
+    liItems.forEach((item) => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: item.refID,
+          start: "top center",
+          end: "bottom center",
+          toggleActions: "play none none none",
+          onEnter: () => {
+            // Reset all list items to their initial color
+            Object.values(liRefs).forEach((ref) => {
+              gsap.to(ref.current, { color: "initial" });
+            });
+
+            // Set the color of the current list item to red
+            gsap.to(liRefs[item.item].current, { color: "red" });
+          },
+          onEnterBack: () => {
+            // Reset all list items to their initial color
+            Object.values(liRefs).forEach((ref) => {
+              gsap.to(ref.current, { color: "initial" });
+            });
+
+            // Set the color of the current list item to red
+            gsap.to(liRefs[item.item].current, { color: "red" });
+          },
+        },
+      });
+
+      return () => {
+        tl.kill();
+      };
+    });
+  }, [liRefs]);
+
+  return (
+    <>
+      <div>
+        <div className="bg-red-300 flex justify-center gap-40 h-20 items-center fixed w-full">
+          {liItems.map((item) => (
+            <div key={item.id} className="text-6xl" ref={liRefs[item.item]}>
+              <div>{item.item}</div>
+            </div>
+          ))}
+        </div>
+
+        <div id="homeRef" className="bg-red-500 h-[100vh]">
+          home
+        </div>
+        <div id="aboutRef" className="bg-blue-500 h-[100vh]">
+          about
+        </div>
+        <div id="contactRef" className="bg-green-500 h-[100vh]">
+          contact
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
